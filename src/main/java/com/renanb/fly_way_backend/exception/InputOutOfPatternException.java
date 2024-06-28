2f0d767f969a98d8601f6ce5f7ceda21fd761323
RenanBilhan <renan.bilhan@hotmail.com>
2024-06-27 21:44:44 -0300

    Feat exception handling and validation config

--- src/main/java/com/renanb/fly_way_backend/exception/InputOutOfPatternException.java
+++
@@ -1,7 +0,0 @@
-package com.renanb.fly_way_backend.exception;
-
-public class InputOutOfPatternException extends RuntimeException {
-    public InputOutOfPatternException(){
-        super("Input out of pattern exception.");
-    }
-}
--- src/main/java/com/renanb/fly_way_backend/model/TripAddress.java
+++ src/main/java/com/renanb/fly_way_backend/model/TripAddress.java
@@ -1,22 +1,26 @@
 package com.renanb.fly_way_backend.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 import jakarta.validation.constraints.Pattern;
+import org.springframework.format.annotation.NumberFormat;
 
 
 @Table(name = "trip_address")
 public record TripAddress (
         @Id
         Long idTripAddress,
-        @NotBlank(message = "Country must be informed.")
+        @NotBlank(message = "{country.not.blank}")
         String country,
 
-        @NotBlank(message = "Street or avenue must be informed.")
+        @NotBlank(message = "{streetOrAvenue.not.blank}")
         String streetOrAvenue,
 //        @Pattern(regexp = "\\d{5}\\d{3}", message = "Zip code must be in the format XXXXXXXX")
-        @NotBlank(message = "Zip code must be informed.")
+        @NotBlank(message = "{zipCode.not.blank}")
+        @Pattern(regexp = "\\d{5}\\d{3}", message = "{zipCode.not.blank}")
         String zipCode
 ){
 
