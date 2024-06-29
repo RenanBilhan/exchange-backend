72f0a82518ac36da51f60e6e8e1736dc27482992
RenanBilhan <renan.bilhan@hotmail.com>
2024-06-28 22:50:41 -0300

    Validation inside trip address handler

--- src/main/java/com/renanb/fly_way_backend/handler/TripAddressHandler.java
+++ src/main/java/com/renanb/fly_way_backend/handler/TripAddressHandler.java
@@ -5,14 +5,22 @@ import com.renanb.fly_way_backend.model.TripAddress;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.rsocket.RSocketProperties;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
+@Validated
 public class TripAddressHandler {
+
+    private final Validator validator;
 
     private final TripAddressRepository tripAddressRepository;
 
@@ -26,8 +34,16 @@ public class TripAddressHandler {
         return ServerResponse.ok().body(response, TripAddress.class);
     }
     public Mono<ServerResponse> save(ServerRequest request){
-        Mono<TripAddress> newAddress = request.bodyToMono(TripAddress.class);
+        Mono<TripAddress> newAddress = request.bodyToMono(TripAddress.class).doOnNext(this::validate);
         Mono<TripAddress> response = newAddress.flatMap(address -> tripAddressRepository.save(address));
         return ServerResponse.ok().body(response, TripAddress.class);
+    }
+
+    private void validate(TripAddress address) {
+        Errors errors = new BeanPropertyBindingResult(address, "address");
+        validator.validate(address, errors);
+        if (errors.hasErrors()) {
+            throw new ServerWebInputException(errors.toString());
+        }
     }
 }
