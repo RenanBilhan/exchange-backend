0fa8906be4b66319b60e5dc514692a9fb5212085
RenanBilhan <renan.bilhan@hotmail.com>
2024-07-07 14:26:27 -0300

    Trip repository

--- src/main/java/com/renanb/fly_way_backend/repository/TripRepository.java
+++ src/main/java/com/renanb/fly_way_backend/repository/TripRepository.java
@@ -1,4 +1,9 @@
 package com.renanb.fly_way_backend.repository;
 
-public interface TripRepository {
+import com.renanb.fly_way_backend.model.Trip;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TripRepository extends ReactiveCrudRepository<Trip, Long> {
 }
